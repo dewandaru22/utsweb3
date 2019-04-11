d9bb89333a15f25ca51c79a289bf1bee03b588e8
dewandaru22 <stevenaji94@gmail.com>
2019-04-11 08:53:36 +0700

    Updates

---
+++ precache-manifest.f7da3835ac2e7148db2f7cdb554b7fe9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/utsweb3/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "371dd3676839bd68e7ae",
+    "url": "/utsweb3/static/js/runtime~main.0bfe0192.js"
+  },
+  {
+    "revision": "da2d78b6ddb1756fdd67",
+    "url": "/utsweb3/static/js/main.4741b14d.chunk.js"
+  },
+  {
+    "revision": "6c9e394e00f05efc2c5e",
+    "url": "/utsweb3/static/js/2.a9b953c7.chunk.js"
+  },
+  {
+    "revision": "da2d78b6ddb1756fdd67",
+    "url": "/utsweb3/static/css/main.83e2c02a.chunk.css"
+  },
+  {
+    "revision": "c00587a66b18718a5fc675b925573373",
+    "url": "/utsweb3/index.html"
+  }
+];
